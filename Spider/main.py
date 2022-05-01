afe1ba7605a33c610b73399ed4ce4746296bd23b
fnardn <56334451+niniubiwoc@users.noreply.github.com>
2022-05-01 20:13:41 +0800

    Update main.py

--- Spider/main.py
+++ Spider/main.py
@@ -26,7 +26,7 @@ options.add_argument('--disable-dev-shm-usage')
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 
-lists = "4gaerghaerageagwrgare"
+lists = "ga3rehtesqgeafgeqav"
 
 def getmidstring(html, start_str, end):
     start = html.find(start_str)
